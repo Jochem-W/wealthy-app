6052d2d50787f6ffd2e6a935abcfd97c070b634a
Jochem-W <16637577+Jochem-W@users.noreply.github.com>
2023-06-01 17:41:24 +0200

    Add title and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,12 @@
 import "./globals.css"
 import { Inter } from "next/font/google"
+import { Metadata } from "next"
 
 const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  title: "Suspiciously Wealthy Furries",
+}
 
 // eslint-disable-next-line react/function-component-definition
 export default function RootLayout({
