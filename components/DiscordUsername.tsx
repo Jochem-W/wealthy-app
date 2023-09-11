a27da8beb02afde2351db439003dafc23415511f
Jochem-W <16637577+Jochem-W@users.noreply.github.com>
2023-09-11 15:08:26 +0200

    Show discrimator if non-zero

--- components/DiscordUsername.tsx
+++ components/DiscordUsername.tsx
@@ -1,6 +1,13 @@
 import { RawDiscordUsername } from "./RawDiscordUsername"
 import { APIUser } from "discord-api-types/v10"
 
-export const DiscordUsername = ({ user: {username} }: { readonly user: APIUser }) => (
-  <RawDiscordUsername username={username}></RawDiscordUsername>
+export const DiscordUsername = ({
+  user: { username, discriminator },
+}: {
+  readonly user: APIUser
+}) => (
+  <RawDiscordUsername
+    username={username}
+    discriminator={discriminator}
+  ></RawDiscordUsername>
 )
