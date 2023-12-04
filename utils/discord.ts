a06a77d503cc86f318afe9250d9fc10629913c46
Jochem-W <16637577+Jochem-W@users.noreply.github.com>
2023-12-05 00:00:26 +0100

    Get rid of invites

--- utils/discord.ts
+++ utils/discord.ts
@@ -1,6 +1,4 @@
-import { getServerSession, Session } from "next-auth"
-import { Options } from "@/app/api/auth/[...nextauth]/route"
-import { APIGuildMember, APIUser, Routes } from "discord-api-types/v10"
+import { APIGuildMember, APIUser } from "discord-api-types/v10"
 import { Variables } from "@/utils/variables"
 import { REST } from "@discordjs/rest"
 
@@ -11,21 +9,6 @@ const globalForDiscord = global as unknown as { discord: REST | undefined }
 export const Discord =
   globalForDiscord.discord ??
   new REST({ version: "10" }).setToken(Variables.discordBotToken)
-
-export async function checkMember(existingSession?: Session) {
-  const session = existingSession ?? (await getServerSession(Options))
-  if (!session?.user) {
-    return false
-  }
-
-  try {
-    await Discord.get(Routes.guildMember(Variables.guildId, session.user.id))
-  } catch (e) {
-    return false
-  }
-
-  return true
-}
 
 if (process.env.NODE_ENV !== "production") {
   globalForDiscord.discord = Discord
