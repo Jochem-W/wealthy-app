c5b7f47c388a320369b34b3e592a9c954641f67f
Jochem-W <16637577+Jochem-W@users.noreply.github.com>
2024-02-20 22:49:00 +0100

    Add names and header

--- app/birthdays/page.tsx
+++ app/birthdays/page.tsx
@@ -6,6 +6,7 @@ import {
   APIUser,
   Routes,
   RESTGetAPIGuildMembersResult,
+  RESTGetAPIGuildResult,
 } from "discord-api-types/v10"
 import { DateTime } from "luxon"
 import Image from "next/image"
@@ -25,9 +26,17 @@ const monthNames = [
   "December",
 ]
 
+function displayName(user: APIUser) {
+  return user.global_name ?? user.username
+}
+
 export const revalidate = 3600
 
 export default async function Page() {
+  const guild = (await Discord.get(
+    Routes.guild(Variables.guildId),
+  )) as RESTGetAPIGuildResult
+
   const users = new Map<string, APIUser>()
 
   const members = (await Discord.get(Routes.guildMembers(Variables.guildId), {
@@ -59,9 +68,12 @@ export default async function Page() {
     birthdays.get(key)?.push(user)
   }
 
-  let date = DateTime.fromObject({ year: 2024, month: 1, day: 1 }).startOf(
-    "week",
-  )
+  for (const value of birthdays.values()) {
+    value.sort((a, b) => displayName(a).localeCompare(displayName(b)))
+  }
+  const year = DateTime.now().year
+
+  let date = DateTime.fromObject({ year, month: 1, day: 1 }).startOf("week")
 
   const months: (DateTime<true> | DateTime<false>)[][][] = [
     [],
@@ -78,7 +90,7 @@ export default async function Page() {
     [],
   ]
 
-  while (date.year !== 2025) {
+  while (date.year !== year + 1) {
     const week = []
     do {
       week.push(date)
@@ -92,13 +104,16 @@ export default async function Page() {
     }
 
     months[startMonth - 1]?.push(week)
-    if (date.year !== 2025 && startMonth !== endMonth) {
+    if (date.year !== year + 1 && startMonth !== endMonth) {
       months[endMonth - 1]?.push(week)
     }
   }
 
   return (
     <section className="flex flex-col gap-4">
+      <h1 className="text-center text-3xl">
+        {guild.name} Birthday Calendar {year}
+      </h1>
       {months.map((month, i) => {
         return (
           <section key={`m${i}`} className="flex flex-col items-center gap-2">
@@ -116,23 +131,32 @@ export default async function Page() {
                   return (
                     <section
                       key={`${date.year}${date.month}${date.day}`}
-                      className={`flex w-full flex-col items-center ${date.month !== i + 1 ? "opacity-15" : "before:opacity-15"} ${date.weekday === 7 ? "text-red-400" : ""} gap-1 before:block before:h-px before:w-full before:bg-white`}
+                      className={`flex w-full flex-col items-center ${date.month !== i + 1 ? "opacity-15" : "before:opacity-15"} gap-1 before:block before:h-px before:w-full before:bg-white`}
                     >
-                      <h3>{date.day}</h3>
-                      <section className="flex min-h-16 flex-wrap items-start justify-center gap-1">
+                      <h3 className={date.weekday === 7 ? "text-red-400" : ""}>
+                        {date.day}
+                      </h3>
+                      <section className="flex min-h-16 max-w-full flex-col items-start gap-1">
                         {birthdays
                           .get(`${date.month}-${date.day}`)
                           ?.map((user) => {
                             return (
-                              <Image
-                                title={user.global_name ?? user.username}
-                                className="rounded-full"
-                                alt={`${user.global_name ?? user.username}'s avatar`}
+                              <section
                                 key={`${i}-${user.id}`}
-                                src={displayAvatarUrl(user)}
-                                width={24}
-                                height={24}
-                              ></Image>
+                                title={displayName(user)}
+                                className="w-15 flex max-w-full gap-1"
+                              >
+                                <Image
+                                  className="rounded-full"
+                                  alt={`${displayName(user)}'s avatar`}
+                                  src={displayAvatarUrl(user)}
+                                  width={24}
+                                  height={24}
+                                ></Image>
+                                <span className="overflow-hidden text-ellipsis whitespace-nowrap">
+                                  {displayName(user)}
+                                </span>
+                              </section>
                             )
                           })}
                       </section>
