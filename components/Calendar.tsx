bf555796f497573c791c53c927ebeee86b57d3e6
Jochem-W <16637577+Jochem-W@users.noreply.github.com>
2024-02-25 20:34:31 +0100

    Make calendar horizontal

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -4,7 +4,7 @@ import { displayAvatarUrl, displayName } from "@/utils/discordClient"
 import { APIGuild, APIUser } from "discord-api-types/v10"
 import { DateTime } from "luxon"
 import Image from "next/image"
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 const monthNames = [
   "January",
@@ -55,9 +55,23 @@ export default function Calendar({
   const [today, setToday] = useState(
     DateTime.fromMillis(initialToday, { zone: "utc" }),
   )
+  const scrollRef = useRef<HTMLDivElement>(null)
+  const todayRef = useRef<HTMLDivElement>(null)
 
+  function listener(evt: WheelEvent) {
+    scrollRef.current?.scrollBy({ left: evt.deltaY, behavior: "instant" })
+  }
+
+  // Set the date/time to the current time on load
   useEffect(() => setToday(DateTime.now()), [])
 
+  // Scroll the calendar horizontally if the user scrolls vertically
+  useEffect(() => {
+    window.addEventListener("wheel", listener)
+    return () => window.removeEventListener("wheel", listener)
+  }, [])
+
+  // Update the date/time automatically
   useEffect(() => {
     const timeout = setTimeout(
       () => setToday(DateTime.now()),
@@ -67,68 +81,84 @@ export default function Calendar({
     return () => clearTimeout(timeout)
   }, [today])
 
+  // Scroll to the current date on load
+  useEffect(() => todayRef.current?.scrollIntoView(), [todayRef])
+
   const months = getYear(today.year)
 
   return (
-    <section className="flex flex-col gap-4">
+    <>
       <h1 className="text-center text-3xl">
         {guild.name} Birthday Calendar {today.year}
       </h1>
-      {months.map((month, i) => (
-        <section key={`m${i}`} className="flex flex-col items-center gap-2">
-          <h2 className="text-2xl">{monthNames[i]}</h2>
-          <section className="grid w-full max-w-4xl grid-cols-7 justify-items-center">
-            <span>M</span>
-            <span>T</span>
-            <span>W</span>
-            <span>T</span>
-            <span>F</span>
-            <span>S</span>
-            <span className="text-red-400">S</span>
-            {month.map((week) =>
-              week.map((date) => (
-                <section
-                  key={`${date.year}-${date.month}-${date.day}`}
-                  className={`${date.month !== i + 1 ? "opacity-15" : "before:opacity-15"} ${date.hasSame(today, "day") ? "bg-neutral-100 dark:bg-neutral-950" : ""} flex w-full flex-col items-center gap-1 before:block before:h-px before:w-full before:bg-black dark:before:bg-white`}
-                >
-                  <h3
-                    className={date.weekday === 7 ? "text-red-400" : undefined}
+      <section
+        ref={scrollRef}
+        className="container flex snap-x snap-mandatory overflow-x-scroll"
+      >
+        {months.map((month, i) => (
+          <section
+            key={`m${i}`}
+            className="flex w-full shrink-0 snap-center flex-col items-center gap-2"
+          >
+            <h2 className="text-2xl">{monthNames[i]}</h2>
+            <section className="grid w-full grid-cols-7 justify-items-center">
+              <span>M</span>
+              <span>T</span>
+              <span>W</span>
+              <span>T</span>
+              <span>F</span>
+              <span>S</span>
+              <span className="text-red-400">S</span>
+              {month.map((week) =>
+                week.map((date) => (
+                  <section
+                    ref={date.hasSame(today, "day") ? todayRef : undefined}
+                    key={`${date.year}-${date.month}-${date.day}`}
+                    className={`${date.month !== i + 1 ? "opacity-15" : "before:opacity-15"} ${date.hasSame(today, "day") ? "bg-neutral-100 dark:bg-neutral-950" : ""} flex w-full flex-col items-center gap-1 before:block before:h-px before:w-full before:bg-black dark:before:bg-white`}
                   >
-                    {date.day}
-                  </h3>
-                  <section className="flex min-h-16 max-w-full flex-col items-start gap-1 px-1 pb-2 text-xs sm:text-base">
-                    {birthdays.get(`${date.month}-${date.day}`)?.map((user) => (
-                      <section
-                        key={`${i}-${user.id}`}
-                        title={displayName(user)}
-                        className="w-15 flex max-w-full items-center gap-1"
-                      >
-                        <Image
-                          className="rounded-full sm:hidden"
-                          alt={`${displayName(user)}'s avatar`}
-                          src={displayAvatarUrl(user)}
-                          width={16}
-                          height={16}
-                        ></Image>
-                        <Image
-                          className="hidden rounded-full sm:block"
-                          alt={`${displayName(user)}'s avatar`}
-                          src={displayAvatarUrl(user)}
-                          width={24}
-                          height={24}
-                        ></Image>
-                        <span className="overflow-hidden text-clip whitespace-nowrap sm:text-ellipsis">
-                          {displayName(user)}
-                        </span>
-                      </section>
-                    ))}
+                    <h3
+                      className={
+                        date.weekday === 7 ? "text-red-400" : undefined
+                      }
+                    >
+                      {date.day}
+                    </h3>
+                    <section className="p flex min-h-16 max-w-full flex-col items-start gap-1 px-1 text-xs sm:text-base">
+                      {birthdays
+                        .get(`${date.month}-${date.day}`)
+                        ?.map((user) => (
+                          <section
+                            key={`${i}-${user.id}`}
+                            title={displayName(user)}
+                            className="w-15 flex max-w-full items-center gap-1"
+                          >
+                            <Image
+                              className="rounded-full sm:hidden"
+                              alt={`${displayName(user)}'s avatar`}
+                              src={displayAvatarUrl(user)}
+                              width={16}
+                              height={16}
+                            ></Image>
+                            <Image
+                              className="hidden rounded-full sm:block"
+                              alt={`${displayName(user)}'s avatar`}
+                              src={displayAvatarUrl(user)}
+                              width={24}
+                              height={24}
+                            ></Image>
+                            <span className="overflow-hidden text-clip whitespace-nowrap sm:text-ellipsis">
+                              {displayName(user)}
+                            </span>
+                          </section>
+                        ))}
+                    </section>
                   </section>
-                </section>
-              )),
-            )}
+                )),
+              )}
+            </section>
           </section>
-        </section>
-      ))}
-    </section>
+        ))}
+      </section>
+    </>
   )
 }
