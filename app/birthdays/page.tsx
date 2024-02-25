ee257c6364370a61d918ea4fb926ea10fd13e5f2
Jochem-W <16637577+Jochem-W@users.noreply.github.com>
2024-02-25 17:18:32 +0100

    Remove year from title

--- app/birthdays/page.tsx
+++ app/birthdays/page.tsx
@@ -20,7 +20,7 @@ export async function generateMetadata(): Promise<Metadata> {
     Routes.guild(Variables.guildId),
   )) as RESTGetAPIGuildResult
 
-  return { title: `${guild.name} | Birthday Calendar ${DateTime.now().year}` }
+  return { title: `${guild.name} | Birthday Calendar` }
 }
 
 async function getBirthdays() {
